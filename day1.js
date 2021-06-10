894bb83acbe130a8d2788891271eac3856775be6
jessduff <jess.tasstic@gmail.com>
2021-06-10 09:49:35 -0400

    Day1 Part2

--- day1.js
+++ day1.js
@@ -1,5 +1,5 @@
-var floor = 0;
 function santaDelivery(dir) {
+    var floor = 0;
     for (var i = 0; i < dir.length; i++) {
         if (dir.charAt(i) == '(') {
             floor++;
@@ -9,6 +9,9 @@ function santaDelivery(dir) {
             } else {
                 console.log("Error");
             }
+        }
+        if (floor == -1) {
+            console.log("Enter the basement in position " + (i+1));
         }
     }
     return floor;
